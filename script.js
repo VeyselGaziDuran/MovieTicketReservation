a9754725b2d27bd4643633367bbfc3b144fe9c0a
Veysel Gazi Duran <veyselduran82@gmail.com>
2023-06-05 19:27:36 +0300

    Added price update according to selected seat number and time

--- script.js
+++ script.js
@@ -1,12 +1,22 @@
 const container = document.querySelector('.container');
 const count = document.getElementById('count');
-
+const select = document.getElementById('movie');
 
 container.addEventListener('click', function(e) {
     if(e.target.classList.contains('seat') && !e.target.classList.contains('reserved')) {
         e.target.classList.toggle('selected');
-
-       let selectedSeatCount = container.querySelectorAll('.seat.selected').length-1;
-       count.innerText = selectedSeatCount;
+        calculateTotal();
     }
 });
+
+
+select.addEventListener('change', function(e) {
+    calculateTotal();
+});
+
+function calculateTotal() {
+    let selectedSeatCount = container.querySelectorAll('.seat.selected').length-1;
+    let price = select.value;
+    count.innerText = selectedSeatCount;
+    amount.innerText = price*(selectedSeatCount);
+}
